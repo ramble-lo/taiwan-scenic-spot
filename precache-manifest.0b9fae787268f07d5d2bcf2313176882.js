e145bdde307310e93e3b1809e18d92fb9c3cab53
羅士欽 <s490607@gmail.com>
2021-12-24 20:44:34 +0800

    Updates

---
+++ precache-manifest.0b9fae787268f07d5d2bcf2313176882.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08d0ecdb9d891fd857cb",
+    "url": "/taiwan-scenic-spot/static/css/main.364c4b8b.chunk.css"
+  },
+  {
+    "revision": "08d0ecdb9d891fd857cb",
+    "url": "/taiwan-scenic-spot/static/js/main.08d0ecdb.chunk.js"
+  },
+  {
+    "revision": "20b57b28324e05a957d1",
+    "url": "/taiwan-scenic-spot/static/js/1.20b57b28.chunk.js"
+  },
+  {
+    "revision": "9c5902659b30c6d65531",
+    "url": "/taiwan-scenic-spot/static/js/runtime~main.9c590265.js"
+  },
+  {
+    "revision": "6023eaf86b77e8db4daf18927d911b72",
+    "url": "/taiwan-scenic-spot/index.html"
+  }
+];
